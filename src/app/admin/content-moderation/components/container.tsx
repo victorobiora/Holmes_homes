e0d55dba9757c51130dc5411ea80caecc949a91b
Victor Obiora <victorobioratech@gmail.com>
2025-03-14 23:39:38 +0100

    check 6

--- src/app/admin/content-moderation/components/container.tsx
+++ src/app/admin/content-moderation/components/container.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from "react";
 import { Sidebar } from "../../generalcomponents/admin-sidebar";
-
+import ReviewsMainSection from "./main-section";
 
 const Container = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -16,7 +16,7 @@ const Container = () => {
 			<Sidebar openMenu={isOpen} setOpenMenu={setIsOpen} />
 			{/* Main content */}
 			<div className="flex-1 h-screen overflow-auto">
-				
+				<ReviewsMainSection toggleSidebar={toggleSidebar} />
 			</div>
 		</div>
 	);
